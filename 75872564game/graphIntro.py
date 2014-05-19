7453937a410597a31d11862190a0415205097c12
huiwu_liu <huiwu_liu@huiwu.liu-ThinkPad-T430>
2014-05-19 16:07:27 +0800

    graphics test

---
+++ 75872564game/graphIntro.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''A simple graphics example constructs a face from basic shapes.
+'''
+
+from graphics import *
+
+
+def main():
+    win = GraphWin('Face', 200, 150) # give title and dimensions
+    #win.yUp() # make right side up coordinates!
+
+    head = Circle(Point(40,100), 25) # set center and radius
+    head.setFill("yellow")
+    head.draw(win)
+
+    eye1 = Circle(Point(30, 105), 5)
+    eye1.setFill('blue')
+    eye1.draw(win)
+
+    eye2 = Line(Point(45, 105), Point(55, 105)) # set endpoints
+    eye2.setWidth(3)
+    eye2.draw(win)
+
+
+    eye3 = Line(Point(85, 125), Point(25, 125)) # set endpoints
+    eye3.setWidth(3)
+    eye3.draw(win)
+
+
+    mouth = Oval(Point(30, 90), Point(50, 85)) # set corners of bounding box
+    mouth.setFill("red")
+    mouth.draw(win)
+
+    label = Text(Point(100, 120), 'A face')
+    label.draw(win)
+
+    message = Text(Point(win.getWidth()/2, 20), 'Click anywhere to quit.')
+    message.draw(win)
+    win.getMouse()
+    win.close()
+
+main()
---
+++ 75872564game/testgraphics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from graphics import *
+
+def main():
+    win = GraphWin("My Circle", 100, 100)
+    c = Circle(Point(50,50), 10)
+    c.draw(win)
+    win.getMouse() # pause for click in window
+    win.close()
+
+main()
