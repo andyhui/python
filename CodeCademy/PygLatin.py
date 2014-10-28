ba53d9118ed54169b9bc33824dea2afcdc8f31fc
huiwu_liu <huiwu_liu@huiwu.liu-ThinkPad-T430>
2014-10-28 10:18:35 +0800

    add some test codes

---
+++ CodeCademy/PygLatin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+print 'welcome to the Pig Latin Translator'
+
+raw_input("Enter a word:")
+original = raw_input()
+if len(original) > 0:
+    print original
+else:
+    print "empty "
---
+++ test/lcp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import pdb
+
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        if not strs :
+            return ""
+        lcp_str = strs[0]
+        for i in range(0,len(strs)):
+            pdb.set_trace()
+            if len(strs[i]) == 0 or len(lcp_str) == 0:
+                return ""
+            lcp_len = len(strs[i]) if len(strs[i]) <= len(lcp_str) else len(lcp_str)
+            for j in range(0,lcp_len):
+                if not lcp_str[j] == strs[i][j]:
+                    break
+                else:
+                    j = j + 1
+            #j = j + 1
+            lcp_str = strs[i][:j]
+        return lcp_str
+
+a = Solution()
+str = a.longestCommonPrefix(["a","b","abc"])
+print str
